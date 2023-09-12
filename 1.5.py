7302abb3c8574d6a635b43a33cf6de54d0aeadb3
AsapBook <vito.puk.1910@mail.ru>
2023-09-13 00:00:56 +0300

    lab1

--- 1.5.py
+++ 1.5.py
@@ -1,6 +1,9 @@
 import os
-my_dict = {"Кольцо": ("Серебро", 1500, 5), "Колье": ("Золото", 4399, 10), "Цепь": ("Серебро", 1488, 13), "Подвеска": ("Золото", 7899, 3), }
-a = my_dict.items()
+
+jewelery = {"Rolex": ["Платина", 300000, 1],
+            "Кольцо": ["Золото", 1150, 5],
+            "Подвеска": ["Серебро", 270, 7]}
+my_basket = 0
 while 1:
     print("1. Просмотр описания: название – описание\n"
           "2. Просмотр цены: название – цена\n"
@@ -10,21 +13,44 @@ while 1:
           "6. Выход\n")
     i = input()
     if i == '1':
-        for i in a:
-            print(f"{i[0]} - {i[1][0]}")
+        for item, info in jewelery.items():
+            print(f"{item} – {info[0]}")
     elif i == '2':
-        for i in a:
-            print(f"{i[0]} - {i[1][1]}р")
+        for item, info in jewelery.items():
+            print(f"{item} – {info[1]}")
     elif i == '3':
-        for i in a:
-            print(f"{i[0]} - {i[1][2]} шт")
+        for item, info in jewelery.items():
+            print(f"{item} – {info[2]}")
     elif i == '4':
-        for i in a:
-            print(f"{i[0]} - {i[1][0]} - {i[1][1]}р - {i[1][2]} шт")
+        for item, info in jewelery.items():
+            print(f"{item} – {info[0]}, Цена: {info[1]} руб., Количество: {info[2]} шт.")
     elif i == '5':
-        print(1)
+        if my_basket != 0:
+            print(f"Вы купили вещей на {my_basket} р.")
+        good = input("Введите желаемый к покупке товар\n")
+        if good in jewelery:
+            amount = input("Введите количество товара\n")
+            if amount == 'n':
+                break
+            elif not amount.isdigit():
+                print("Неверное количество")
+            else:
+                amount = int(amount)
+                if amount > jewelery[good][2]:
+                    print("Товара в таком количестве нет в наличии")
+                elif amount == jewelery[good][2]:
+                    my_basket = jewelery[good][1] * amount
+                    del jewelery[good]
+                else:
+                    my_basket = jewelery[good][1] * amount
+                    jewelery[good][2] -= amount
+            break
+        else:
+            print("Данного товара нет в наличии")
     elif i == '6':
         os.system('cls')
+        if my_basket != 0:
+            print(f"Вы купили вещей на {my_basket} р.")
         print("До свидания")
         break
     else:
